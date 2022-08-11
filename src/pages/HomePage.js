6665182d7818a820b3bcfb775c06276863e80ace
Hamid Hassani <hamidhassani119@gmail.com>
2022-08-12 03:50:48 +0430

    some bugs fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(`/signup`);
+  });
+
   return (
     <div>
       <h1>Welcome to home page!.</h1>
